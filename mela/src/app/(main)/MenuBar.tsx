7ea7d9cad3ed5d92e75e459069b8facc26520c84
cigerxwinislam <islamtunc748@gmail.com>
2024-12-22 07:32:13 +0300

    serxere nu

---
+++ mela/src/app/(main)/MenuBar.tsx
@@ -0,0 +1,58 @@
+// Bismillahirahmanirahim
+
+
+
+import { validateRequest } from "@/auth";
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import { Bookmark, Home } from "lucide-react";
+import Link from "next/link";
+import NotificationsButton from "./NotificationsButton";
+
+interface MenuBarProps {
+  className?: string;
+}
+
+export default async function MenuBar({ className }: MenuBarProps) {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  const unreadNotificationsCount = await Promise.all([
+    prisma.notification.count({
+      where: {
+        recipientId: user.id,
+        read: false,
+      },
+    }),
+
+  ]);
+
+  return (
+    <div className={className}>
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Home"
+        asChild
+      >
+        <Link href="/">
+          <Home />
+          <span className="hidden lg:inline">Malper</span>
+        </Link>
+      </Button>
+    
+      <Button
+        variant="ghost"
+        className="flex items-center justify-start gap-3"
+        title="Bêrika we"
+        asChild
+      >
+        <Link href="/bookmarks">
+          <Bookmark />
+          <span className="hidden lg:inline">Bêrik</span>
+        </Link>
+      </Button>
+    </div>
+  );
+}
