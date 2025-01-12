de8bccac2d7e1efa8f953573ec50b9f14ecb85f2
cigerxwinislam <islamtunc748@gmail.com>
2025-01-12 16:20:38 -0500

    serxere nu

--- mela/src/app/(main)/Navbar.tsx
+++ mela/src/app/(main)/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
         <Link href="/" className="text-2xl font-bold text-primary">
 
- Müşterisi Burada
+ Yekazad Yazılım
         </Link>
         <SearchField />
         <UserButton className="sm:ms-auto" />
--- mela/src/app/(main)/mmavahi/page.tsx
+++ mela/src/app/(main)/mmavahi/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
 
-        <h1 className="text-3xl font-semibold">Emlak</h1>
+        <h1 className="text-3xl font-semibold">Malper</h1>
         <Tabs defaultValue="mm">
           <TabsList>
-            <TabsTrigger value="for-you">İlanlar</TabsTrigger>
+            <TabsTrigger value="for-you">Parvekirin</TabsTrigger>
             <TabsTrigger value="following">Kategoride Ara</TabsTrigger>
-            <TabsTrigger value="mm">Yeni ilan Ver</TabsTrigger>
+            <TabsTrigger value="mm">Parve Bike</TabsTrigger>
 
           </TabsList>
           <TabsContent value="for-you">
