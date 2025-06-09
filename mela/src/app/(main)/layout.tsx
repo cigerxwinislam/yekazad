2713a0e531efba5dca7d31c463a44aeec8048efc
cigerxwinislam <islamtunc748@gmail.com>
2025-06-09 12:42:11 +0300

    serxere nu

--- mela/src/app/(main)/layout.tsx
+++ mela/src/app/(main)/layout.tsx
@@ -1,7 +1,12 @@
 // Bismillahirahmanirahim 
+// Elhamdulillahirabbulalemin
+// Esselatu vesselamu ala rasulillah ve ala alihi ve sahbihi ecmain
+// Allahumme salli ala seyyidina Muhammedin ve ala alihi ve sahbihi ecmain
+// Allah u Ekber, Allahu Ekber, Allahu Ekber
+// La ilahe illallah, Allahu Ekber, Allahu Ekber, ve lillahi'l-hamd
 
 
-
+import React from "react";
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import MenuBar from "./MenuBar";
